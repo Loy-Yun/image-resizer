6d52c7162df9058ce837fbd134877d0b3101d9b3
Loy-Yun <yoonsoji@gmail.com>
2021-11-10 01:32:19 +0900

    최소 사이즈 200, 400, 600

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const AWS = require('aws-sdk');
 const sharp = require('sharp');
 
 const s3 = new AWS.S3();
+const transforms = [
+  { name: "s200", size: 200 },
+  { name: "s400", size: 400 },
+  { name: "s600", size: 600 }
+];
 
-exports.handler = function(event, context, callback) {
+exports.handler = async (event, context, callback) => {
 
   // 이벤트를 발생시킨 버킷 이름 & 객체 키 값
   let srcBucket = event.Records[0].s3.bucket.name;
   let srcKey    = event.Records[0].s3.object.key;
   // 저장할 버킷 & 키 값
   let dstBucket = srcBucket;
-  let dstKey    = srcKey.replace(/origin/g, 'w200');
+  // let dstKey    = srcKey.replace(/origin/g, 'w200');
 
   // 이미지 타입 예외처리
   var typeMatch = srcKey.match(/\.([^.]*)$/);
@@ -26,53 +31,43 @@ exports.handler = function(event, context, callback) {
     return;
   }
 
-  async.waterfall([
-    // 이미지 가져오기
-      function download(next) {
-        s3.getObject({
-            Bucket: srcBucket,
-            Key: srcKey
-          },
-          next);
-      },
-    // 이미지 리사이징
-      function transform(response, next) {
-        sharp(response.Body)
-          .resize({width: 200}) // 사이즈: 가로 200
-          .withMetadata()	// 이미지 exif 데이터 유지
-          .toBuffer(function(err, buffer) {
-            if (err) {
-              next(err);
-            } else {
-              next(null, response.ContentType, buffer);
-            }
-          });
-      },
-    // 결과물 업로드
-      function upload(contentType, data, next) {
-        s3.putObject({
+  try {
+    const image = await s3.getObject({ Bucket: srcBucket, Key: srcKey }).promise();
+
+    await Promise.all(
+      transforms.map(async item => {
+        let dstKey = srcKey.replace(/origin/g, item.name);
+
+        const resizeWidth = await sharp(image.Body).metadata().then(function(metadata) {
+          let imgHeight = metadata.height; //이미지 높이
+          let imgWidth = metadata.width; //이미지 넓이
+
+          let scalingFactor = Math.max(
+            item.size / imgWidth,
+            item.size / imgHeight
+          );
+          return Math.floor(scalingFactor * imgWidth);
+        });
+
+        console.log(dstKey,' resize width ',resizeWidth)
+
+        const resizedImg = await sharp(image.Body)
+          .resize({ width: resizeWidth })
+          .toFormat("png")
+          .png({ quality: 100 })
+          .toBuffer();
+        return await s3
+          .putObject({
             Bucket: dstBucket,
+            Body: resizedImg,
             Key: dstKey,
-            Body: data,
-            ContentType: contentType,
             ACL: 'public-read'
-          },
-          next);
-      }
-    ], function (err) {
-      if (err) {
-        console.error(
-          'Unable to resize ' + srcBucket + '/' + srcKey +
-          ' and upload to ' + dstBucket + '/' + dstKey +
-          ' due to an error: ' + err
-        );
-      } else {
-        console.log(
-          'Successfully resized ' + srcBucket + '/' + srcKey +
-          ' and uploaded to ' + dstBucket + '/' + dstKey
-        );
-      }
-      callback(null, "message");
-    }
-  );
+          })
+          .promise();
+      })
+    );
+    callback(null, `Success: ${srcKey}`);
+  } catch (err) {
+    callback(`Error resizing files: ${err}`);
+  }
 };
